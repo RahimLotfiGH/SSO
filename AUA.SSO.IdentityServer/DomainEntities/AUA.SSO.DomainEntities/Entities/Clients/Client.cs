8b5ea7a786f2d1caefa53e1278a4346c2e299f12
Rahim Lotfi <Mr_lotfi@ymail.com>
2025-03-21 01:18:54 +0330

    add-refactor

--- AUA.SSO.IdentityServer/DomainEntities/AUA.SSO.DomainEntities/Entities/Clients/Client.cs
+++ AUA.SSO.IdentityServer/DomainEntities/AUA.SSO.DomainEntities/Entities/Clients/Client.cs
@@ -3,10 +3,10 @@ using AUA.SSO.DomainEntities.BaseRepo;
 
 namespace AUA.SSO.DomainEntities.Entities.Clients
 {
-    public class Client:BaseEntity<int>
+    public class Client : BaseEntity<int>
     {
         public string ClientId { get; set; }
-        public string InvokerKey { get; set; }
+        //public string InvokerKey { get; set; }
         public bool RequireClientSecret { get; set; } = true;
         public List<ClientGrantType> AllowedGrantTypes { get; set; }
         public List<ClientScope> AllowedScopes { get; set; }
